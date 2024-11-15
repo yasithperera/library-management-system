68be9d581716df118bff436e3e530d56af1f0bc6
Yasith Ishara Perera <yasithishara@gmail.com>
2024-11-16 00:03:32 +0530

    added more css styling

--- src/app/admin/manage-books/manage-books.component.ts
+++ src/app/admin/manage-books/manage-books.component.ts
@@ -3,6 +3,7 @@ import {BookService} from "../../services/book.service";
 import {AuthorService} from "../../services/author.service";
 import {Book} from "../../models/book.model";
 import {Router} from "@angular/router";
+import {Author} from "../../models/author.model";
 
 @Component({
   selector: 'app-manage-books',
@@ -10,8 +11,8 @@ import {Router} from "@angular/router";
   styleUrls: ['./manage-books.component.css'],
 })
 export class ManageBooksComponent implements OnInit {
-  books: any[] = [];
-  authors: any[] = [];
+  books: Book[] = [];
+  authors: Author[] = [];
 
   constructor(
     private bookService: BookService,
@@ -45,5 +46,11 @@ export class ManageBooksComponent implements OnInit {
   // Triggered when the user selects a book to edit
   updateBook(bookId: string): void {
     this.router.navigate([`/update-book`, bookId]);
+  }
+
+  // Get author name by ID
+  getAuthorName(authorId: string): string {
+    const author = this.authors.find((a) => a.id === authorId);
+    return author ? author.name : 'Unknown Author';
   }
 }
